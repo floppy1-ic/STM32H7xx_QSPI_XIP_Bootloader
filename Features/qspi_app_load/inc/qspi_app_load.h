6d12c440214a2207866b1323e70606e1818e4a9c
floppy1-ic <beherasusil14@gmail.com>
2026-05-17 01:56:02 +0530

    Add QSPI application load functionality
    - Introduced qspi_app_load.h and qspi_app_load.c to facilitate loading application binaries into external QSPI flash.

---
+++ Features/qspi_app_load/inc/qspi_app_load.h
@@ -0,0 +1,15 @@
+/**
+  ******************************************************************************
+  * @file    qspi_app_load.h
+  * @author  Sibun
+  * @brief   Load application binary into external QSPI flash.
+  *
+  * @copyright
+  * Copyright (c) 2026 Sibun. All rights reserved.
+  ******************************************************************************
+  */
+
+#ifndef QSPI_APP_LOAD_H
+#define QSPI_APP_LOAD_H
+
+#endif /* QSPI_APP_LOAD_H */
---
+++ Features/qspi_app_load/src/qspi_app_load.c
@@ -0,0 +1,10 @@
+/**
+  ******************************************************************************
+  * @file    qspi_app_load.c
+  * @author  Sibun
+  * @brief   Load application binary into external QSPI flash.
+  *
+  * @copyright
+  * Copyright (c) 2026 Sibun. All rights reserved.
+  ******************************************************************************
+  */
