48df48da9bfd37bb3868c30410c25eec4b674857
floppy1-ic <beherasusil14@gmail.com>
2026-05-18 20:17:19 +0530

    Add QSPI application load function
    - Implemented qspi_new_app_load function to manage the application loading process.

--- Features/qspi_app_load/inc/qspi_app_load.h
+++ Features/qspi_app_load/inc/qspi_app_load.h
@@ -12,4 +12,22 @@
 #ifndef QSPI_APP_LOAD_H
 #define QSPI_APP_LOAD_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Stub display time until QSPI program path is implemented */
+#define QSPI_APP_LOAD_STUB_DISPLAY_MS    (5000U)
+
+/**
+  * @brief  Run app-load session (program external flash when implemented).
+  * @param  None
+  * @retval None (does not return; soft reset after stub or real load).
+  */
+void qspi_new_app_load(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* QSPI_APP_LOAD_H */
--- Features/qspi_app_load/src/qspi_app_load.c
+++ Features/qspi_app_load/src/qspi_app_load.c
@@ -8,3 +8,26 @@
   * Copyright (c) 2026 Sibun. All rights reserved.
   ******************************************************************************
   */
+
+#include "qspi_app_load.h"
+#include "app_shared_ram.h"
+#include "lcd_mcal.h"
+#include "main.h"
+
+/**
+  * @brief  Run app-load session (program external flash when implemented).
+  * @param  None
+  * @retval None (does not return; soft reset after stub or real load).
+  */
+void qspi_new_app_load(void)
+{
+  lcd_stm32h7_clear();
+  lcd_stm32h7_color(BLK, BLE);
+  lcd_stm32h7_message("App load : Wait...\n");
+
+  /* TODO: QSPI_Flash_DisableMemoryMappedMode, erase/write app.bin @ 0 */
+  HAL_Delay(QSPI_APP_LOAD_STUB_DISPLAY_MS);
+
+  app_load_disable();
+  NVIC_SystemReset();
+}
